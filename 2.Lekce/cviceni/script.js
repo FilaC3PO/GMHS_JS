a63c3d85553887eaf58148ebd5401d4994473fd9
FilaC3PO <krttluk@gmail.com>
2026-02-13 10:56:07 +0100

    13.02,26

---
+++ 2.Lekce/cviceni/script.js
@@ -0,0 +1,26 @@
+alert("zkouška");
+alert(3);
+
+//proměnné pro jména
+
+let userName = Filip
+console.log(userName);
+
+userNamel = "violka" 
+console.log(userName);
+
+//proměnné pro věk obou lidí
+
+let ageL = 17;
+console.log(ageL)
+let ageF = 16;
+console.log(ageF)
+
+// součet věků
+
+let sucet = ageL + ageF;
+console.log(soucet)
+
+//věty s oběma jmény a součtem
+
+let text = userName + "a" + userName + "mají" + soucet + "let."
