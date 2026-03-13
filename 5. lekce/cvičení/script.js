f3c9d5fd56e545f07a7b2c35b07c766bf8a89a80
FilaC3PO <krttluk@gmail.com>
2026-03-13 10:39:36 +0100

    13.3.26

--- 5. lekce/cvičení/script.js
+++ 5. lekce/cvičení/script.js
@@ -7,4 +7,26 @@ let price;
 // 1) Základní cena
 if (age < 3) {
     price = 0;
+} else if (isTuesday) {
+    price = 5;
+} else if (age <= 12) {
+    price = 5;
+} else if (age <= 17) {
+    price = 7;
+} else if (age <= 64) {
+    price = 10;
+} else {
+    price = 6;
 }
+
+// 2) Sleva věrnostní kartou (neplatí v úterý ami pro zdarma)
+if (price > 0 && !isTuesday && hasLoyaltyCard) {
+    price -= 2;
+}
+
+// 3) Cena nesmí být záporná
+if (price < 0) {
+    price = 0;
+}
+
+console.log("Cena lístku je $" + price);
