3d1bf98f6ee6ce9579399bbe0d41c997be709d6a
FilaC3PO <krttluk@gmail.com>
2026-04-24 10:29:27 +0200

    24.4.26

--- 3.3.7/app.js
+++ 3.3.7/app.js
@@ -34,6 +34,9 @@ test(123)
 
 function scitani(num1, num2) {
     console.log(num1 + num2)
+    console.log(num1 - num2)
+    console.log(num1 * num2)
+    console.log(num1 / num2)
 }
 
 scitani(4,6)
@@ -55,3 +58,21 @@ function deleni(num1, num2) {
 }
 
 deleni(24,6)
+
+
+// Napiš funkci printRange, která vypíše do
+// konzole všechna čísla od parametru start až po end
+// Postup:
+// 1) Deklarace funkce
+// 2) definování parametrů
+// 3) for smyčka
+// 4) Vyvolání funkce a argumentů
+
+function printRange(start, end) {
+  for (let i = start; i <= end; i++) {
+    console.log(i);
+  }
+}
+ 
+printRange(3, 7);
+printRange(10, 15)
