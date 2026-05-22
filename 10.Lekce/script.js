a792e0f5752ceebe728a5c5ecaad7cbd3ac97b49
FilaC3PO <krttluk@gmail.com>
2026-05-22 10:36:12 +0200

    22.5.

---
+++ 10.Lekce/script.js
@@ -0,0 +1,12 @@
+//Deklarace funkce
+// Cyklus for
+// console.log(i);
+
+// VOlání funkce
+function printRange(start, end) {
+    for (let i = start; i <= end; i++){
+        console.log(i);
+    }
+}
+
+printRange(1,4)
